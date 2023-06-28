75c99cb61f7a34a387063506d9e711490668cafc
Serhii131313 <serhii.tarasov.skan@gmail.com>
2023-06-28 15:54:19 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,40 +1,15 @@
 import throttle from "lodash.throttle";
 
-const STORAGE_KEY = "feedback-form-state";
+const iframe = document.querySelector("#vimeo-player");
+const CURRENT_TIME = "videoplayer-current-time";
+const player = new Vimeo.Player(iframe);
+player.on("timeupdate", throttle(onPlay, 1000));
 
-const form = document.querySelector(".feedback-form");
-
-form.addEventListener("input", throttle(onInputData, 500));
-form.addEventListener("submit", onFormSubmit);
-
-let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-const { email, message } = form.elements;
-reloadPage();
-
-function onInputData(event) {
-    dataForm = { email: email.value, message: message.value };
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+function onPlay(data) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(data));
 }
-
-function reloadPage() {
-    if (dataForm) {
-        email.value = dataForm.email || "";
-        message.value = dataForm.message || "";
-    }
-}
-function onFormSubmit(e) {
-    e.preventDefault();
-    console.log({ email: email.value, message: message.value });
-
-    if (email.value === "" || message.value === "") {
-        return alert(`Будь ласка, заповніть всі обов'язкові поля.`);
-    }
-
-    localStorage.removeItem(STORAGE_KEY);
-    e.currentTarget.reset();
-}
-function onFormvalue(e) {
-    feedback.emeil = e.currentTarget.value;
-
-    localStorage.setItem(feedback_form, JSON.stringify(feedback));
+const localStorageGet = localStorage.getItem(CURRENT_TIME);
+const pars = JSON.parse(localStorageGet);
+if (localStorageGet) {
+    player.setCurrentTime(pars.seconds);
 }
