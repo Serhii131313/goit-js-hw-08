1f1feaced5391ae36974b591503f03b97109aad6
Serhii131313 <serhii.tarasov.skan@gmail.com>
2023-06-28 15:34:27 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import _ from "lodash.throttle";
+const form = document.querySelector(".feedback-form");
+const data = localStorage.getItem("feedback");
+form.addEventListener("input", _(updateStorage, 500));
+form.addEventListener("submit", clearStorage);
+function updateStorage(e) {
+    const local = JSON.parse(localStorage.getItem("feedback")) ?? { email: null, message: null };
+    local[e.target.name] = e.target.value;
+    localStorage.setItem("feedback", JSON.stringify(local))
+}
+function checkStorage() {
+    if (!data) return;
+    const parsed = JSON.parse(data)
+    form.email.value = parsed.email;
+    form.message.value = parsed.message;
+}
+function clearStorage(e) {
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem("feedback")));
+    localStorage.removeItem("feedback");
+    e.target.reset();
+}
+checkStorage();
